f3c22752d874e8279ab8884b76b228ef7b6ff33b
FarahKamleh <fkamleh8800@gmail.com>
2022-09-14 19:32:25 -0500

    Lighting Code Fixed
    The lighting for both cubes has been fixed; the light now successfully turns on and off when turned upside-down without any flickering.

--- Project 428.P1.Farah Kamleh/Assets/GFLighting.cs
+++ Project 428.P1.Farah Kamleh/Assets/GFLighting.cs
@@ -10,26 +10,46 @@ public class GFLighting : MonoBehaviour
     // EDIT: game object for turning on and off light
     public GameObject GFLight;
 
+    // EDIT: use a bool value to determine if cube is right-side-up or up-side-down
+    bool prev;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // EDIT: false means up, true means down; initial state is up, so initial value false
+        prev = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         // EDIT: if the cube is turned up-side-down, change the lighting
-        if (Vector3.Dot(GFCube.transform.up, Vector3.down) > 0) {
+        if ((Vector3.Dot(GFCube.transform.up, Vector3.down) > 0) && (prev == false)) {
             
             // EDIT: if light is off, turn on
             if (GFLight.GetComponent<Light>().enabled == false) {
+
+                // EDIT: turn on light
                 GFLight.GetComponent<Light>().enabled = true;
+
+                // EDIT: set prev to true
+                prev = true;
             }
             // EDIT: if light is on, turn off
             else {
+                // EDIT: turn light off
                 GFLight.GetComponent<Light>().enabled = false;
+
+                // EDIT: set prev to true
+                prev = true;
             }
+        }
+
+        // EDIT: if the cube is right-side-up, change the value of prev to false
+        else if (!(Vector3.Dot(GFCube.transform.up, Vector3.down) > 0) && (prev == true)) {
+
+            // EDIT: set to false
+            prev = false;
         }
     }
 }
--- Project 428.P1.Farah Kamleh/Assets/LuxoJrLighting.cs
+++ Project 428.P1.Farah Kamleh/Assets/LuxoJrLighting.cs
@@ -10,26 +10,47 @@ public class LuxoJrLighting : MonoBehaviour
     // EDIT: game object for turning on and off light
     public GameObject luxoLight;
 
+    // EDIT: use a bool value to determine if cube is right-side-up or up-side-down
+    bool prev;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // EDIT: false means up, true means down; initial state is up, so initial value false
+        prev = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // EDIT: if the cube is turned up-side-down, change the lighting
-        if (Vector3.Dot(pixarCube.transform.up, Vector3.down) > 0) {
+        // EDIT: if the cube has been turned up-side-down, change the lighting
+        if ((Vector3.Dot(pixarCube.transform.up, Vector3.down) > 0) && (prev == false)) {
             
             // EDIT: if light is off, turn on
             if (luxoLight.GetComponent<Light>().enabled == false) {
+
+                // EDIT: turn on light
                 luxoLight.GetComponent<Light>().enabled = true;
+
+                // EDIT: set prev to true
+                prev = true;
             }
             // EDIT: if light is on, turn off
             else {
+
+                // EDIT: turn light off
                 luxoLight.GetComponent<Light>().enabled = false;
+
+                // EDIT: set prev to true
+                prev = true;
             }
+        }
+
+        // EDIT: if the cube is right-side-up, change the value of prev to false
+        else if (!(Vector3.Dot(pixarCube.transform.up, Vector3.down) > 0) && (prev == true)) {
+
+            // EDIT: set to false
+            prev = false;
         }
     }
 }
